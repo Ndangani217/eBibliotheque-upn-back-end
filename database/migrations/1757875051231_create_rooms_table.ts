9243dee4a7be46be6611fb5809185112d323ddd1
Heritier Ndangani <heritierndangani217@gmail.com>
2025-09-15 19:50:19 +0100

    feat: create students table migration with necessary fields and relationships

--- database/migrations/1757875051231_create_rooms_table.ts
+++ database/migrations/1757875051231_create_rooms_table.ts
@@ -1,18 +1,32 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
+import { Status } from '../../app/types/status/index.js'
 
 export default class extends BaseSchema {
-  protected tableName = 'rooms'
+    protected tableName = 'rooms'
 
-  async up() {
-    this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
+    async up() {
+        this.schema.createTable(this.tableName, (table) => {
+            table.string('id').primary()
+            table.string('type').notNullable()
+            table.integer('capacity').notNullable()
+            table
+                .enum('occupancy_status', Object.values(Status), {
+                    useNative: true,
+                    enumName: 'room_status_enum',
+                })
+                .notNullable()
+            table.integer('available_spots').notNullable()
+            table.string('location').notNullable()
+            table.boolean('is_available').defaultTo(true)
+            table.specificType('current_members', 'integer[]').defaultTo('{}')
+            table.string('description', 500)
 
-      table.timestamp('created_at')
-      table.timestamp('updated_at')
-    })
-  }
+            table.timestamp('created_at')
+            table.timestamp('updated_at')
+        })
+    }
 
-  async down() {
-    this.schema.dropTable(this.tableName)
-  }
+    async down() {
+        this.schema.dropTable(this.tableName)
+    }
 }
